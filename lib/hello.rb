f712caffaea37fa6a2e850da0f0b7c58b8231b3d
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-17 21:33:26 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,3 +1,22 @@
+
+
+def hello_t(array)
+ if block_given?
+   i = 0
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  array
+  else
+    puts "Hey! No block was given!"
+  end 
+end
+
+
+=begin
+
 def hello_t(array)
   if block_given?
     i = 0 
@@ -13,7 +32,7 @@ def hello_t(array)
     puts "Hey! No block was given!"
   end 
 end 
-
+=end 
 
 # call your method here!
 
--- test.rb
+++ test.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def yielding_with_arguments(num)
   puts "the program is executing the code inside the method"
   yield(num)
@@ -5,3 +6,15 @@ def yielding_with_arguments(num)
 end
 
 yielding_with_arguments(15) {|i| puts i * 2}
+=======
+
+
+def yielding_things(num)
+  {|i| puts i * 2}
+  puts "this is a thing"
+  yield (num)
+  puts  "this is also a thing"
+end 
+  
+yielding_things(5)
+>>>>>>> 988b45dcf82664917b673192e547d153965aaac3
